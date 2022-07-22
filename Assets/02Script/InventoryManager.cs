06a1afeca5f29474822f743423e5ff24c0f9469c
Jpot777 <psymchoi@gmail.com>
2022-07-22 21:01:47 +0900

    First Commit

---
+++ Assets/02Script/InventoryManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] gunObjs = null;
+
+    void Start()
+    {
+        // for (var i = 0; i < gunObjs.Length; i++)
+        // {
+        //     Instantiate(gunObjs[i]);
+        // }
+    }
+}
